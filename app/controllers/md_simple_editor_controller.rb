2f75d9377b40eb9303b4e8968a186212777f59c1
Matthew Groff <mattlgroff@gmail.com>
2018-01-21 23:27:13 -0800

    Update md_simple_editor_controller.rb
    Removed CSRF Protection

--- app/controllers/md_simple_editor_controller.rb
+++ app/controllers/md_simple_editor_controller.rb
@@ -1,4 +1,7 @@
 class MdSimpleEditorController < ApplicationController
+	# Remove CSRF Protection
+	protect_from_forgery with: :null_session
+	
 	def preview
 		options = {autolink: true, tables: true, hard_wrap: true, no_intra_emphasis: true, fenced_code:true, gh_blockcode: true}
 		html = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options).render(params['md']).html_safe
